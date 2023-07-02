4431e6b50baf83238f254d4aa3a02bb0c746b18c
Asif Alli <alli.asif@gmail.com>
2023-07-02 10:59:55 -0400

    left bounds fine tuning

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -343,7 +343,7 @@ function gameLoop() {
   player.update();
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = player.speed;
-  } else if (keys.left.pressed && player.position.x > 100) {
+  } else if (keys.left.pressed && player.position.x > 100 || keys.left.pressed && scrollOffset === 0 && player.position.x > 0) {
     player.velocity.x = -player.speed;
   } else {
     player.velocity.x = 0;
@@ -355,7 +355,7 @@ function gameLoop() {
       genericObjects.forEach(function (genericObject) {
         genericObject.position.x -= player.speed * 0.66;
       });
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && scrollOffset > 0) {
       scrollOffset -= player.speed;
       platforms.forEach(function (platform) {
         platform.position.x += player.speed;
